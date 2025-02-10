b08eb83f65ff400de11fe037ba384911674ed097
dxdf <tuxttys@gmail.com>
2025-02-10 20:35:27 +0800

    mihomo tproxy renamed to nikki

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,10 +4,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
 # homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
