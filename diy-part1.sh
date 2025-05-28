75c1802ae475cf71e9caf5751431568dc6988aa9
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-05-28 19:16:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,14 +5,14 @@
 
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # homeproxy
-#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
 
 #git clone https://github.com/281677160/openwrt-package package/openwrt-package
-#git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/vernesong/OpenClash package/OpenClash
