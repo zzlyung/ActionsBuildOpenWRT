5019a66dc0ae42682420fa57abdef7c2505439d9
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-05-29 14:00:33 +0800

    Update x86_64-part2.sh

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -2,5 +2,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/luci2/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/luci2/bin/config_generate
