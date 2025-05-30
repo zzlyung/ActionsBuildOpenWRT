4e01c366924536dd98fa63d86f5af207501f1a4c
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-05-30 20:13:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.c
 # homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
 
-git clone https://github.com/281677160/openwrt-package package/openwrt-package
+#git clone https://github.com/281677160/openwrt-package package/openwrt-package
 git clone https://github.com/vernesong/OpenClash package/OpenClash
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
