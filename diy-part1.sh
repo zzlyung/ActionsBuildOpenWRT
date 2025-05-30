ffc6c0fccbd078144fa2c6e6f103d6e577710c53
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-05-30 17:19:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,12 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.c
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
 # homeproxy
-#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
 
 #git clone https://github.com/281677160/openwrt-package package/openwrt-package
 git clone https://github.com/vernesong/OpenClash package/OpenClash
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 git clone https://github.com/BCYDTZ/luci-app-UUGameAcc package/luci-app-UUGameAcc
 git clone https://github.com/Droid-MAX/luci-app-lingtigamebooster package/luci-app-lingtigamebooster
 git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
