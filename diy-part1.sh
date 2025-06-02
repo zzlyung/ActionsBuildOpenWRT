d8fafccf3b97d8106c8fb59df3d9333849390b16
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-06-02 15:15:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,11 @@ sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./fee
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
+# homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+
 git clone https://github.com/vernesong/OpenClash package/OpenClash
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
-
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/BCYDTZ/luci-app-UUGameAcc package/luci-app-UUGameAcc
