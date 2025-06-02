6b3e311e59f0d73fb9dee97b3453514e6edc6194
zzlyung <124063972+zzlyung@users.noreply.github.com>
2025-06-02 15:52:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,7 +6,6 @@
 # Add a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
 # homeproxy
